5b06c3d57a32e9b796eceadbecec8929f3156753
TimeForANinja <TimeForANinja@users.noreply.github.com>
2019-02-11 21:11:30 +0100

    add util.js file

--- index.js
+++ index.js
@@ -1,24 +1,11 @@
 const PATH = require('path');
 const UTIL = require('backend-util');
+const UUTIL = require('./util.js')
 const MINIMIST = require('minimist');
-const READLINE = require('readline');
 const LOGGER = require('backend-logger');
 
 const argv = MINIMIST(process.argv.splice(2));
 const setuppers = UTIL.loader(PATH.resolve(__dirname, './setuppers/'));
-
-const readStdin = (q, validateFN) => new Promise(resolve => {
-  const rl = READLINE.createInterface(process.stdin, process.stdout);
-  rl.setPrompt(`${q}> `);
-  rl.prompt();
-  rl.on('line', line => {
-    if (validateFN(line)) {
-      rl.close();
-      return resolve(line);
-    }
-    return rl.prompt();
-  });
-});
 
 (async function main() {
   if (argv._[0] === 'setup') {
@@ -30,7 +17,7 @@ const readStdin = (q, validateFN) => new Promise(resolve => {
       const padLength = s.concat().sort((a, b) => b.name.length - a.name.length)[0].name.length;
       LOGGER.logClean('available modules:');
       LOGGER.logClean(s.map((a, i) => `${i}: ${a.name.padEnd(padLength)} | ${a.description}`).join('\n'));
-      const selected = await readStdin('module:', line => !!s[Number(line)]);
+      const selected = await UUTIL.readStdin('module:', line => !!s[Number(line)]);
       s[selected].install();
     }
   } else {
---
+++ util.js
@@ -0,0 +1,68 @@
+const READLINE = require('readline');
+const URL = require('url');
+
+exports.mysqlRegex = /^[$a-zA-Z0-9_]+$/
+const hostnameRegex = exports.hostnameRegex = /^@$|^[a-zA-Z]+([-a-zA-Z.][a-zA-Z]+)*$|^([0-9]{1,3}\.){2}[0-9]{1,3}$///
+const isBool = exports.isBool = line => ['y', 'n'].includes(line);
+
+exports.parseEndpoint = line => {
+    const parts = line.split(',');
+    return {
+      hostname: parts[0],
+      allowUnsecure: parts[1] === 'y',
+    };
+  };
+
+exports.isEndpoint = line => {
+    const parts = line.split(',');
+    if (!isBool(parts[1])) return false;
+    if (!parts[0].match(hostnameRegex)) return false;
+    return true;
+  };
+
+const isUInt = exports.isUInt = line => !isNaN(line) && Number(line) > 0 && Number(line) === Math.floor(Number(line));
+
+exports.isUIntList = line => !line.split(',').some(a => !isUInt(a));
+
+exports.isClientLocation = line => {
+    const parts = line.split('@');
+    if (!parts[0].match(/^[A-Z]+$/)) return false;
+    const u = URL.parse(`https://${parts[1]}`);
+    if (!u.hostname) return false;
+    if (!u.path) return false;
+    return true;
+  };
+
+exports.parseClientLocation = line => {
+    const parts = line.split('@');
+    const u = URL.parse(`https://${parts[1]}`);
+    const a = {
+      hostname: u.hostname,
+      port: u.port,
+      method: parts[0],
+      path: u.path,
+    };
+    if (!a.port) delete a.port;
+    if (!a.url || a.url === '/') delete a.url;
+    return a;
+  };
+
+exports.readStdin = (q, validateFN, def) => new Promise(resolve => {
+    const rl = READLINE.createInterface(process.stdin, process.stdout);
+    rl.setPrompt(`${q}> `);
+    rl.prompt();
+    if(def !== undefined) rl.write(def);
+    rl.on('line', line => {
+      if (validateFN(line)) {
+        rl.close();
+        return resolve(line);
+      }
+      if(def !== undefined) {
+        rl.prompt();
+        return rl.write(def);
+      }
+      else {
+        return rl.prompt();
+      }
+    });
+  });
